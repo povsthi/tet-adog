a536e4ce9ebb234aff54e695bb31f3ed5d7a7823
Thiago Garcia <garciathiago947@.com>
2024-08-13 08:32:23 -0300

    haha

--- frontend/CadastroUsuario.js
+++ frontend/CadastroUsuario.js
@@ -1,24 +1,37 @@
 import React, { useState } from 'react';
-import { Image, View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import CustomTextInput from './components/CustomTextInput'
 
 const CadastroUsuario = ({ navigation }) => {
   console.log("Cadastro feito com sucesso!");
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
+  const [data, setDataNascimento] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [estado, setEstado] = useState('');
+  const [cidade, setCidade] = useState('');
+  const [rua, setRua] = useState('');
+  const [numero, setNumero] = useState('');
   const [senha, setSenha] = useState('');
+  const [confirmarSenha, setConfirmarSenha] = useState('');
   
 
   const Cadastrar = () => {
 
-    if(!nome || !email || !senha) {
+    if(!nome || !email || !senha || !confirmarSenha || !cpf || !estado || !cidade || !rua || !numero) {
       console.log("Todos os campos são obrigatórios!");
+      return;
+    }
+
+    if (senha !== confirmarSenha) {
+      Alert.alert("Erro!", "Senhas diferentes");
       return;
     }
 
     var userObj = { nome: nome, email: email, senha: senha };
     var jsonBody = JSON.stringify(userObj);
     console.log(jsonBody);
-    fetch('http:localhost:3000/usuarios', {
+    fetch('http://200.18.141.93:3000/usuarios', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -38,31 +51,21 @@ const CadastroUsuario = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Image source={require('./assets/LogoAdog.png')} style={styles.logo} />
-      <TextInput
-        style={styles.input}
-        placeholder="Nome"
-        onChangeText={(text) => setNome(text)}
-        value={nome}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        onChangeText={(text) => setEmail(text)}
-        value={email}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Senha"
-        onChangeText={(text) => setSenha(text)}
-        value={senha}
-        secureTextEntry
-      />
-      <TouchableOpacity onPress={Cadastrar}>
-        <Text style={styles.button}>
-          Cadastrar
-        </Text>
-      </TouchableOpacity>
+      <ScrollView>
+        <CustomTextInput label="Nome" placeholder="Digite seu nome" value={nome} onChangeText={setNome} />
+        <CustomTextInput label="E-mail" placeholder="Digite seu e-mail" value={email} onChangeText={setEmail} />
+        <CustomTextInput label="Data de Nascimento" placeholder="Digite sua data de nascimento" value={data} onChangeText={setDataNascimento} />
+        <CustomTextInput label="CPF" placeholder="Digite seu CPF" value={cpf} onChangeText={setCpf} />
+        <CustomTextInput label="Estado" placeholder="Digite seu estado" value={estado} onChangeText={setEstado} />
+        <CustomTextInput label="Cidade" placeholder="Digite sua cidade" value={cidade} onChangeText={setCidade} />
+        <CustomTextInput label="Rua" placeholder="Digite sua rua" value={rua} onChangeText={setRua} />
+        <CustomTextInput label="Número" placeholder="Digite o número" value={numero} onChangeText={setNumero} keyboardType="numeric" />
+        <CustomTextInput label="Senha" placeholder="Digite sua senha" value={senha} onChangeText={setSenha} secureTextEntry />
+        <CustomTextInput label="Confirmar Senha" placeholder="Confirme sua senha" value={confirmarSenha} onChangeText={setConfirmarSenha} secureTextEntry />
+        <TouchableOpacity onPress={Cadastrar}>
+          <Text style={styles.button}>Cadastrar</Text>
+        </TouchableOpacity>
+      </ScrollView>  
     </View>
   );
 };
---
+++ frontend/components/CustomTextInput.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+const CustomTextInput = ({ label, placeholder, value, onChangeText, secureTextEntry, keyboardType }) => {
+  return (
+    <View style={styles.container}>
+      {label && <Text style={styles.label}>{label}</Text>}
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+        keyboardType={keyboardType}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 20, 
+  },
+  label: {
+    fontSize: 16,
+    marginTop: 3,
+    color: '#333', 
+  },
+  input: {
+    color: "black",
+    borderRadius: 30,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    textAlign: 'center',
+    width: '80%',
+  },
+});
+
+export default CustomTextInput;
